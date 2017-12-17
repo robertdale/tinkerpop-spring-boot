b555a0f59135357e409ac7929a90e16da1c16776
Robert Dale <Robert.Dale@xyleminc.com>
2017-12-17 06:39:22 -0500

    added connection pool test

--- tinkerpop-spring-boot-sample/src/test/java/com/github/robertdale/spring/boot/tinkerpop/sample/TinkerModernGraphTest.java
+++ tinkerpop-spring-boot-sample/src/test/java/com/github/robertdale/spring/boot/tinkerpop/sample/TinkerModernGraphTest.java
@@ -6,16 +6,21 @@ import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.tinkerpop.gremlin.driver.Client;
+import org.apache.tinkerpop.gremlin.driver.Cluster;
 import org.apache.tinkerpop.gremlin.driver.Result;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.util.function.Lambda;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -26,11 +31,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @TestPropertySource(locations = "classpath:application-test.properties")
 public class TinkerModernGraphTest {
 
+    private final Logger log = LoggerFactory.getLogger(TinkerModernGraphTest.class);
+
     @Autowired
     private GraphTraversalSource g;
 
     @Autowired
     private Client client;
+
+    @Autowired
+    private Cluster cluster;
 
     @Test
     public void shouldFindGraph() {
@@ -56,6 +66,33 @@ public class TinkerModernGraphTest {
         assertThat(results, notNullValue());
         assertThat(results, not(empty()));
         results.stream().forEach(System.out::println);
+    }
+
+    @Test
+    public void connectionTests() {
+
+        List<Thread> threads = new LinkedList<>();
+
+        for (int i = 0; i < 20; i++) {
+            final int threadnum = i;
+            Runnable runnable = () -> {
+                log.info("[{}] Starting sleepy call ", threadnum);
+                g.V().map(Lambda.function("Thread.sleep(1000); return it;")).toList();
+                log.info("[{}] Done", threadnum);
+            };
+            Thread thread = new Thread(runnable);
+            threads.add(thread);
+            thread.start();
+        }
+
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        });
     }
 
 }
